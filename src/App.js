26e422eed8ff2a385ee7cc4ff908f4b1516cf343
Cosmic-rare <ysolakey1116@gmail.com>
2022-08-13 14:18:28 +0900

    Clean Project

--- src/App.js
+++ src/App.js
@@ -1,19 +1,11 @@
 import React from 'react';
-import { Router } from "@reach/router"
-
-import Home from './pages/home';
 import './styles/style.css'
-import TopPicks from './pages/top-picks';
 
-class App extends React.Component {
-	render() {
-		return (
-      <Router>
-        <Home path="/"/>
-        <TopPicks path="top-picks"/>
-      </Router>
-		);
-	}
+const App = () => {
+  return (
+    <h1>Hello!</h1>
+  );
+
 }
 
 export default App;
